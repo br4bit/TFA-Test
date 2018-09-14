c219683c5cdfd7d64e6a5da2b7f8be6dee71f3cd
Sal <luxifero@live.it>
2018-09-14 19:02:07 +0200

    Fixed interface

--- TFA Test/Form1.Designer.cs
+++ TFA Test/Form1.Designer.cs
@@ -31,70 +31,194 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(79, 232);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(280, 167);
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(406, 59);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(181, 20);
+            this.textBox1.Size = new System.Drawing.Size(186, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Full Name";
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(430, 144);
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(72, 151);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(157, 49);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Generate OTPAUTH";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(390, 245);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(155, 20);
-            this.textBox2.TabIndex = 3;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(109, 269);
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Location = new System.Drawing.Point(100, 153);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(98, 47);
             this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
+            this.button2.Text = "Login";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(331, 385);
+            this.panel1.TabIndex = 5;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.HidePromptOnLeave = true;
+            this.maskedTextBox1.Location = new System.Drawing.Point(6, 64);
+            this.maskedTextBox1.Mask = "AAAAAAAA";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.PromptChar = ' ';
+            this.maskedTextBox1.Size = new System.Drawing.Size(186, 20);
+            this.maskedTextBox1.TabIndex = 2;
+            this.maskedTextBox1.UseSystemPasswordChar = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.groupBox2);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(378, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(295, 385);
+            this.panel2.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(66, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 90);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Password";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.maskedTextBox3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.maskedTextBox2);
+            this.groupBox2.Location = new System.Drawing.Point(65, 31);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(177, 85);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "TOTP";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.AllowPromptAsInput = false;
+            this.maskedTextBox2.HidePromptOnLeave = true;
+            this.maskedTextBox2.InsertKeyMode = System.Windows.Forms.InsertKeyMode.Overwrite;
+            this.maskedTextBox2.Location = new System.Drawing.Point(6, 59);
+            this.maskedTextBox2.Mask = "AAAAAAAA";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.PromptChar = ' ';
+            this.maskedTextBox2.Size = new System.Drawing.Size(155, 20);
+            this.maskedTextBox2.TabIndex = 6;
+            this.maskedTextBox2.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.AllowPromptAsInput = false;
+            this.maskedTextBox3.HidePromptOnLeave = true;
+            this.maskedTextBox3.InsertKeyMode = System.Windows.Forms.InsertKeyMode.Overwrite;
+            this.maskedTextBox3.Location = new System.Drawing.Point(6, 19);
+            this.maskedTextBox3.Mask = "999 999";
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.PromptChar = ' ';
+            this.maskedTextBox3.Size = new System.Drawing.Size(155, 20);
+            this.maskedTextBox3.TabIndex = 7;
+            this.maskedTextBox3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.maskedTextBox3_MouseClick);
+            this.maskedTextBox3.Enter += new System.EventHandler(this.maskedTextBox3_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(117, 268);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(723, 460);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "TFA Test";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -103,8 +227,17 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- TFA Test/Form1.cs
+++ TFA Test/Form1.cs
@@ -13,17 +13,31 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TwoFactorAuthNet;
+using System.Security.Cryptography;
+using System.Reflection;
 
 namespace TFA_Test
 {
     public partial class Form1 : Form
     {
         TwoFactorAuth tfa;
-        String secret;
+        String secret = "BTPF7XOLCHPMW76KXJX54TVVAACTHZYZ";
         public Form1()
         {
             InitializeComponent();
-
+            tfa = new TwoFactorAuth("TFA Test", qrcodeprovider: new MyQRProvider());
+            // string curFile = @"Auth.txt";
+            // Console.WriteLine(File.Exists(curFile) ? "File exists." : "File does not exist.");
+            if (File.Exists(@"Auth.txt"))
+            {
+                panel1.Hide();
+                panel2.Location = panel1.Location;
+            }
+            else
+            {
+                panel2.Hide();
+                panel1.Show();
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -31,30 +45,96 @@ namespace TFA_Test
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Button1_Click(object sender, EventArgs e)
         {
             string testo = textBox1.Text;
-            tfa = new TwoFactorAuth("TFA Test", qrcodeprovider: new MyQRProvider());
-            secret = tfa.CreateSecret(160);
-            var uri = tfa.QrCodeProvider.GetQrCodeImage(String.Format("otpauth://totp/{0}?secret={1}&issuer=TFA Test", textBox1.Text, secret), 250);
+            
+            //secret = tfa.CreateSecret(160);
+            var uri = tfa.QrCodeProvider.GetQrCodeImage(String.Format("otpauth://totp/{0}?secret={1}&issuer=TFA Test", "sasa", secret), 150);
             //Console.WriteLine(System.Text.UTF8Encoding.UTF8.GetString(uri));
             Image x = (Bitmap)((new ImageConverter()).ConvertFrom(uri));
             pictureBox1.Image = x;
+            var validate = maskedTextBox1.Text.ToLower();
+            Console.WriteLine(validate);
         }
-
+        static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
         private void button2_Click(object sender, EventArgs e)
         {
+          
             CheckQrCode(tfa, secret);
+            /*byte[] bytes = Encoding.ASCII.GetBytes(maskedTextBox1.Text);
+            Console.WriteLine(Encoding.ASCII.GetByteCount(maskedTextBox1.Text));
+            foreach(Byte b in bytes)
+            {
+                Console.WriteLine(b);
+            }
+            using (AesManaged myAes = new AesManaged())
+            {
+                myAes.Mode = CipherMode.ECB;
+
+                myAes.IV = bytes;
+                Console.WriteLine(myAes.IV.Length);
+                myAes.Key = Encoding.ASCII.GetBytes(secret);
+                Console.WriteLine(myAes.Key.Length);
+                ICryptoTransform encryptor = myAes.CreateEncryptor(myAes.Key, myAes.IV);
+
+                using (CryptoStream csEncrypt = new CryptoStream(new MemoryStream(), encryptor, CryptoStreamMode.Write))
+                {
+                    csEncrypt.Write(Encoding.ASCII.GetBytes("ciao come stai"), 0, 13);
+                    Console.WriteLine(csEncrypt.ToString());
+                }
+
+            }*/
+        }
+        public byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        //cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+                
+            }
+
+            return encryptedBytes;
         }
         private void CheckQrCode(TwoFactorAuth tfa, string secret)
         {
             Console.WriteLine(((DateTimeOffset)GetTimeServer()).ToUnixTimeSeconds());
-            if (tfa.VerifyCode(secret, textBox2.Text, 1, timestamp: ((DateTimeOffset)GetTimeServer()).ToUnixTimeSeconds() + 30)) //Code must be enter in 30 seconds before expire on google auth.
+            if (tfa.VerifyCode(secret, maskedTextBox3.Text.Replace(maskedTextBox3.PromptChar.ToString(),""), 1, timestamp: ((DateTimeOffset)GetTimeServer()).ToUnixTimeSeconds() + 30)) //Code must be enter in 30 seconds before expire on google auth.
             {
-                Console.WriteLine("true");
+                    label3.Text="Login Successful";
+                    Console.WriteLine("true");
             }
             else
             {
+                label3.Text = "Password Timeout";
                 Console.WriteLine("false");
             }
         }
@@ -74,6 +154,25 @@ namespace TFA_Test
             {
                 return DateTime.Now; //In case something goes wrong. 
             }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "Full Name")
+                textBox1.Clear();
+            textBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+        }
+
+        private void maskedTextBox3_Enter(object sender, EventArgs e)
+        {
+            maskedTextBox3.Focus();
+            maskedTextBox3.SelectionStart = 0;
+        }
+
+        private void maskedTextBox3_MouseClick(object sender, MouseEventArgs e)
+        {
+            maskedTextBox3.Focus();
+            maskedTextBox3.SelectionStart = 0;
         }
     }
        /* public static DateTime GetServerTime()
